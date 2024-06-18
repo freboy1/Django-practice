66bc80a197a1fa8c62458004f6f41b11dc7524fb
Alisher <dautovalisher33@gmail.com>
2024-06-18 22:03:32 +0500

    Add list_display(question_text, pub_date) to QuestionAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -11,7 +11,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
-
+    list_display = ['question_text', 'pub_date']
     inlines = [ChoiceInline]
 
 
