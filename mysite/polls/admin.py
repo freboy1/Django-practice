20ca1be0dfc698aa5f2eef84d2dc31389f33decf
Alisher <dautovalisher33@gmail.com>
2024-06-18 22:19:53 +0500

    Add list_filter & Add display decorator to Model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']})
     ]
     list_display = ['question_text', 'pub_date', 'was_published_recently']
+    list_filter = ['pub_date']
     inlines = [ChoiceInline]
 
 
